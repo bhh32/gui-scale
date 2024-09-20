c8baf5df028ceb0411c1716acdb9dbf0f9e1fccb
Bryan Hyland <bryan.hyland32@gmail.com>
2024-09-20 14:43:44 -0700

    Updated binary installer with tailscale set --operator=bryan

--- bin_install_script.sh
+++ bin_install_script.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash -e
 
-## Setup
+# Setup
 read -s -p "Enter sudo password to place the files in the appropriate places: " pass
 
-## Download the file from GitHub
+# Ensure that tailscale is setup to use the program
+sudo tailscale set --operator=$USER
+
+# Download the file from GitHub
 cd ~/Downloads
 wget https://github.com/bhh32/gui-scale/releases/download/0.2.0/gui-scale-0.2.0.tar.gz
 
-## Unpack the tarball
+# Unpack the tarball
 tar xf gui-scale-0.2.0.tar.gz
 
-## Move the unpacked files to their perspective places
+# Move the unpacked files to their perspective places
 echo $pass | sudo -S mv gui-scale-0.2.0/gui-scale /usr/bin/
 sudo mv gui-scale-0.2.0/gui-scale.desktop /usr/share/applications/
 sudo mv gui-scale-0.2.0/tailscale-icon.png /usr/share/pixmaps/
@@ -18,5 +21,5 @@ sudo mv gui-scale-0.2.0/tailscale-icon.png /usr/share/pixmaps/
 # Let the user know it has been installed
 echo "GUI-Scale has been installed successfully!"
 
-## Clean up
+# Clean up
 rm -rf gui-scale-0.2.0 gui-scale-0.2.0.tar.gz bin_install_script.sh
