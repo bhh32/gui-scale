f35348e3816d7195eb3565921fd05c2749c8ed74
Bryan Hyland <bryan.hyland32@gmail.com>
2024-09-20 14:39:20 -0700

    Updated binary install script

--- bin_install_script.sh
+++ bin_install_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -e
 
 ## Setup
-read -s -p "Enter sudo password to place the files in the appropriate places:" pass
+read -s -p "Enter sudo password to place the files in the appropriate places: " pass
 
 ## Download the file from GitHub
 cd ~/Downloads
@@ -11,12 +11,12 @@ wget https://github.com/bhh32/gui-scale/releases/download/0.2.0/gui-scale-0.2.0.
 tar xf gui-scale-0.2.0.tar.gz
 
 ## Move the unpacked files to their perspective places
-sudo -S $pass mv gui-scale /usr/bin/
-sudo -S $pass mv gui-scale.desktop /usr/share/applications/
-sudo -S $pass mv tailscale-icon.png /usr/share/pixmaps/
+echo $pass | sudo -S mv gui-scale-0.2.0/gui-scale /usr/bin/
+sudo mv gui-scale-0.2.0/gui-scale.desktop /usr/share/applications/
+sudo mv gui-scale-0.2.0/tailscale-icon.png /usr/share/pixmaps/
 
 # Let the user know it has been installed
 echo "GUI-Scale has been installed successfully!"
 
 ## Clean up
-rm gui-scale-0.2.0.tar.gz bin_install_script.sh
+rm -rf gui-scale-0.2.0 gui-scale-0.2.0.tar.gz bin_install_script.sh
